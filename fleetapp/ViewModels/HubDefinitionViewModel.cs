1fdad484887770993adfb82994fc466830ffe8b7
Rohan Ghosh <rohan.dummbi@gmail.com>
2018-10-14 02:33:08 +0530

    added dummy datagrid

--- fleetapp/ViewModels/HubDefinitionViewModel.cs
+++ fleetapp/ViewModels/HubDefinitionViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using Caliburn.Micro;
+using fleetapp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace fleetapp.ViewModels
 {
     public class HubDefinitionViewModel : Screen
     {
+        public BindableCollection<PersonModel> People { get; set; }
+
+        public HubDefinitionViewModel()
+        {
+            DataAccess da = new DataAccess();
+            People = new BindableCollection<PersonModel>(da.GetPeople());
+        }
     }
 }
