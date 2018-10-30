379efc3b0f95c5d66182dc4bd3d3e78c3c55daa4
Arpan <rohandumbi@gmail.com>
2018-10-30 23:59:58 +0530

    Added scenario model and data access class

---
+++ fleetapp/DataAccessClasses/ScenarioDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using fleetapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fleetapp.DataAccessClasses 
+{
+    
+    public class ScenarioDataAccess : BaseDataAccess
+    {
+        
+        public List<ScenarioModel> GetScenarios()
+        {
+            using (IDbConnection connection = getConnection()) 
+            {
+                return connection.Query<ScenarioModel>($"select * from Scenario where ProjectID = { Context.ProjectID } ").ToList();
+            }
+        }
+
+        public Boolean InsertScenario(String scenarioName, int startYear, int timePeriod)
+        {
+            using (IDbConnection connection = getConnection())
+            {
+                String insertQuery = $"insert into Scenario (ProjectID, Name, StartYear, TimePeriod ) " +
+                    $"values ('{ Context.ProjectID }', '{ scenarioName }', { startYear } , { timePeriod } )";
+                connection.Execute(insertQuery);
+            }
+            return true;
+        }
+    }
+    
+}
---
+++ fleetapp/Models/ScenarioModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fleetapp.Models
+{
+    public class ScenarioModel
+    {
+        public int id { get; set; }
+        public int ProjectID { get; set; }
+        public String Name { get; set; }
+        public int StartYear { get; set; }
+        public int TimePeriod { get; set; }
+    }
+}
