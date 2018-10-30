f25e0dca129ae33a604733ec9345c7f34f43e8f0
Arpan <rohandumbi@gmail.com>
2018-10-30 23:31:35 +0530

    Code formatting and removing unnecessary comments

--- fleetapp/ViewModels/FleetListViewModel.cs
+++ fleetapp/ViewModels/FleetListViewModel.cs
@@ -15,7 +15,6 @@ namespace fleetapp.ViewModels
         private FleetDataAccess _fleetDataAccess;
         private String _fleetFileName;
         public BindableCollection<FleetModel> Fleets { get; set; }
-        
 
         public FleetListViewModel()
         {
@@ -26,6 +25,7 @@ namespace fleetapp.ViewModels
         private void LoadFleetList()
         {
             Fleets = new BindableCollection<FleetModel>(_fleetDataAccess.GetFleets());
+
         }
 
         public String FleetFile
@@ -44,16 +44,11 @@ namespace fleetapp.ViewModels
 
         private IEnumerable<FleetModel> ReadCSV(string fileName)
         {
-            // We change file extension here to make sure it's a .csv file.
-            // TODO: Error checking.
             string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
 
-            // lines.Select allows me to project each line as a FleetModel. 
-            // This will give me an IEnumerable<FleetModel> back.
             return lines.Select(line =>
             {
                 string[] data = line.Split(',');
-                // We return a person with the data in order.
                 return new FleetModel { ProjectID = Context.ProjectID, AssetType = data[0], AssetModel = data[1], FleetID = data[2] };
             });
         }
