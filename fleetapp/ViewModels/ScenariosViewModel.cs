1ab84568a37d48ffb4526c18b72e0fda09695c96
Rohan Ghosh <rohan.dummbi@gmail.com>
2018-11-02 23:59:06 +0530

    1. removed italic styling in shell view header. 2. added UI lemenst for scenario creation

--- fleetapp/ViewModels/ScenariosViewModel.cs
+++ fleetapp/ViewModels/ScenariosViewModel.cs
@@ -4,10 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace fleetapp.ViewModels
 {
     public class ScenariosViewModel : Screen
     {
+        private String _scenarioName;
+        private int _startYear;
+        private int _timePeriod;
+
+        public String ScenarioName
+        {
+            set { _scenarioName = value; }
+        }
+
+        public int StartYear
+        {
+            set { _startYear = value; }
+        }
+
+        public int TimePeriod
+        {
+            set { _timePeriod = value; }
+        }
+
+        public void CreateScenario()
+        {
+            //_projectDAO.InsertProject(_projectName, _projectDescription);
+            //LoadProjects();
+            //NotifyOfPropertyChange("Projects");
+            MessageBox.Show("Create scenario");
+        }
     }
 }
