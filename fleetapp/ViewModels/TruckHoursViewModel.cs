e9662d23ec2c38cf47bef40c85625ef8b2d7104b
Rohan Ghosh <rohan.dummbi@gmail.com>
2018-11-14 16:05:17 +0530

    1. fixed table name in install.sql 2. first draft of truck hour table

--- fleetapp/ViewModels/TruckHoursViewModel.cs
+++ fleetapp/ViewModels/TruckHoursViewModel.cs
@@ -3,9 +3,12 @@ using fleetapp.DataAccessClasses;
 using fleetapp.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace fleetapp.ViewModels
 {
@@ -18,6 +21,19 @@ namespace fleetapp.ViewModels
         {
             da = new TruckHousDataAccess();
             TruckHours = new BindableCollection<TruckHoursModel>(da.GetTruckHours());
+            this.TruckHoursColumns = new ObservableCollection<DataGridColumn>();
+            this.GenerateDefaultColumns();
+        }
+
+        public ObservableCollection<DataGridColumn> TruckHoursColumns { get; private set; }
+
+        private void GenerateDefaultColumns()
+        {
+            this.TruckHoursColumns.Add(
+                new DataGridTextColumn { Header = "Asset_Model", Binding = new Binding("AssetModel") });
+            this.TruckHoursColumns.Add(new DataGridTextColumn { Header = "Group Name", Binding = new Binding("GroupName") });
+            this.TruckHoursColumns.Add(new DataGridTextColumn { Header = "Hub", Binding = new Binding("Hub") });
+            this.TruckHoursColumns.Add(new DataGridTextColumn { Header = "Mode", Binding = new Binding("Mode") });
         }
     }
 }
